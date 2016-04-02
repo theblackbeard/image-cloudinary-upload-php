d0977395135b276baaf53e59f8fa36973300aec6
Tatiana souza santana <Tatiana souza santana>
2016-04-02 19:44:53 -0300

    Upload Cloudinary PHP

---
+++ resources/views/api/index.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Image Cloud Upload - By Black Beard</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+	<link href="{{ asset('/css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+	<div class="col-xs-12 col-lg-6 col-lg-push-3 janela">
+		<div class="panel panel-default">
+  			<div class="panel-heading">Please, Select the file</div>
+  			<div class="panel-body">
+  				
+  			</div>
+		</div>		
+	</div>
+</body>
+</html>
